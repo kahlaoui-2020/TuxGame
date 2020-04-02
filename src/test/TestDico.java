7c063e67e1aa82d13806532560a55b95d77f4e3a
kahlaoug <kahlaoug@gmail.com>
2020-04-02 14:39:00 +0200

    First commit

---
+++ src/test/TestDico.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import game.Dico;
+import java.io.IOException;
+import java.util.Scanner;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.SAXException;
+
+/**
+ *
+ * @author kahlaoui
+ */
+public class TestDico {
+    
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+     
+        Dico dico = new Dico("") ;
+        /*for(int i = 0; i<5; i++) {
+            
+            Scanner sc = new Scanner(System.in);
+            System.out.println("donner un mot");
+            String mot = sc.nextLine() ;
+            System.out.println("donner son niveau");
+            int niveau = sc.nextInt() ;
+            dico.ajouteMotDico(niveau, mot);
+
+        }*/
+        dico.lireDictionnaireDOM("src/test/","dico.xml");
+        /*System.out.println("Mot niveau 1 : ") ;
+        for(String mot : dico.getList1()) {
+            System.out.println(mot) ;
+        }
+        System.out.println("Mot niveau 2 : ") ;
+        for(String mot : dico.getList2()) {
+            System.out.println(mot) ;
+        }*/
+       /*System.out.println("list1  : "+dico.getMotDepuisListeNiveaux(1)) ;
+       System.out.println("list3  : "+dico.getMotDepuisListeNiveaux(2)) ;
+       System.out.println("list2  : "+dico.getMotDepuisListeNiveaux(2)) ;
+       System.out.println("list1  : "+dico.getMotDepuisListeNiveaux(1)) ;
+       System.out.println("list1  : "+dico.getMotDepuisListeNiveaux(1)) ;*/
+        
+        
+    }
+    
+}
